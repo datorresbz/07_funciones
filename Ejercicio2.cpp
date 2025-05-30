666fdcc8e191fe29c3524d7e33d21a7aa4fb9a09
datorresbz <datorresb@unjbg.edu.pe>
2025-05-29 21:26:18 -0500

    Actializando ejercicio2.cpp: Restriccion de solo numeros que sean positivos

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -4,35 +4,39 @@ using namespace std;
 void divisionConRestas(int num1, int num2) {
     int mayor, menor;
     
-    if(num1 > num2) {
-        mayor = num1;
-        menor = num2;
-    } else {
-        mayor = num2;
-        menor = num1;
-    }
+    if(num1>=0 && num2>=0){
+	
+    	if(num1 > num2) {
+        	mayor = num1;
+        	menor = num2;
+    	} else {
+        	mayor = num2;
+        	menor = num1;
+   		}
     
-    if(menor == 0) {
-        cout << "Error: No se puede dividir por cero" << endl;
-        return;
-    }
-    cout<< "Dividiendo " << mayor << " entre " << menor << " usando restas sucesivas:\n";
-    cout<< "----------------------------------"<<endl;
-    int cociente = 0;
-    int resto = mayor;
+    	if(menor == 0) {
+        	cout << "Error: No se puede dividir por cero" << endl;
+        	return;
+    	}
+    	cout<< "Dividiendo " << mayor << " entre " << menor << " usando restas sucesivas:\n";
+    	cout<< "----------------------------------"<<endl;
+    	int cociente = 0;
+    	int resto = mayor;
     
     
-	while(resto >= menor) {
-        resto = resto - menor;
-        cociente=cociente+1;          
+		while(resto >= menor) {
+        	resto = resto - menor;
+        	cociente=cociente+1;          
+		}
+    	cout << "Resultado final:" << endl;
+    	cout << "Cociente: " << cociente << endl;
+    	cout << "Resto: " << resto << endl;
+	} else{
+		cout<<"Error: Los numeros tienen que ser positivos";
 	}
-    cout << "Resultado final:" << endl;
-    cout << "Cociente: " << cociente << endl;
-    cout << "Resto: " << resto << endl;
 }
-
-int main() {
-    int num1,num2;
+int main(){
+   	int num1,num2;
     
     cout << "Ingrese el primer numero: ";
     cin >> num1;
