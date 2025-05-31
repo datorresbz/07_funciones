24f6e00cf55aa129e64f86d57c7b7a23495d266e
datorresbz <datorresb@unjbg.edu.pe>
2025-05-30 19:27:45 -0500

    Actualiza ejercicio3.cpp: Agregar restriccion que permita solo numeros positivos

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -12,10 +12,20 @@ void SumaFibonacci(int n){
 	int i,numero1,numero2,fibonacci;
 	numero1=0;
 	numero2=1;
-	for(i=1;i<=n;i++){
-		fibonacci=numero1+numero2;
-		cout<<numero1<<"+"<<numero2<<"="<<fibonacci<<endl;
-		numero1=numero2;
-		numero2=fibonacci;
+	if(n>0){
+		for(i=1;i<=n;i++){
+			fibonacci=numero1+numero2;
+			cout<<numero1<<"+"<<numero2<<"="<<fibonacci<<endl;
+			numero1=numero2;
+			numero2=fibonacci;
+		}
+	}
+	else{
+		if(n==0){
+			cout<<"Es igual a 0";
+		}
+		else{
+			cout<<"Error: El numero tiene que ser positivo";
+		}
 	}
 }
