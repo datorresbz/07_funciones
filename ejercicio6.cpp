6abcf65a63aa660799aedc8a911751376cd2ae67
datorresbz <datorresb@unjbg.edu.pe>
2025-06-02 18:47:18 -0500

    Actializa ejercicio6.cpp : Se mejoro al usar numeros negativos(se agrego valor absoluto) y tambien al usar el numero 0

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -3,25 +3,36 @@
 using namespace std;
 
 int mcd(int a, int b) {
+    a = abs(a);
+    b = abs(b);
+    if (a == 0 && b == 0) {
+        return 0;
+    }
     while (b != 0) {
-        int auxiliar = b;
+        int aux = b;
         b = a % b;
-        a = auxiliar;
+        a = aux;
     }
     return a;
 }
-	
+
 int mcm(int a, int b) {
-    return abs(a * b) / mcd(a, b);
+    if (a == 0 || b == 0) {
+        return 0;
+    }
+    return abs((a / mcd(a, b)) * b);
 }
 
-int main(){
-	int num1,num2;
-	cout<<"Ingrese el primer numero: ";
-	cin>>num1;
-	cout<<"Ingrese el segundo numero: ";
-	cin>>num2;
-	cout << "El MCM de " << num1 << " y " << num2 << " es: " << mcm(num1, num2) << endl;
-	cout << "El MCD de " << num1 << " y " << num2 << " es: " << mcd(num1, num2);
+int main() {
+    int num1, num2;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    
+    cout << "El MCM de " << num1 << " y " << num2 << " es: " << mcm(num1, num2) << endl;
+    cout << "El MCD de " << num1 << " y " << num2 << " es: " << mcd(num1, num2);
+    
+    return 0;
 }
 
